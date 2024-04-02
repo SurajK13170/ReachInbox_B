650119fce6d2296f4e0edd965d3d9ffab77dfd72
Suraj Kumar <121710709+SurajK13170@users.noreply.github.com>
2024-04-02 17:11:21 +0530

    done

---
+++ passport.js
@@ -0,0 +1,57 @@
+const passport = require("passport");
+const { User } = require("./models/Google.User.Model");
+const { outlookUser } = require("./models/Outlook.User.model");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const OutlookStrategy = require('passport-outlook').Strategy;
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.ClientID,
+      clientSecret: process.env.ClientSecret,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Google Profile:", profile);
+      profile.tokens = { accessToken, refreshToken };
+      try{
+        const user = User.create({
+          googleId:profile.id,
+          displayName:profile.displayName,
+          email:profile.emails.value,
+          refreshToken:refreshToken,
+          accessToken:accessToken
+        })
+        console.log(user)
+      }catch(err){
+        throw err
+      }
+      return done(null, profile);
+    }
+  )
+);
+
+passport.use(new OutlookStrategy({
+  clientID: process.env.ClientID_Outlook,
+  clientSecret: process.env.ClientSecret_Outlook,
+  callbackURL: "http://localhost:3000/auth/outlook/callback",
+  passReqToCallback: true
+},
+(accessToken, refreshToken, profile, done) => {
+  console.log("Google Profile:", profile);
+  profile.tokens = { accessToken, refreshToken };
+ 
+  return done(null, profile);
+}
+)
+);
+
+module.exports = passport;
